8e5cb9e64e3c37e0a828b59447df14df5d93266f
Karen <karensamanoh@outlook.com>
2019-08-30 16:20:58 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+//Components
+import Navbar from './components/navbar/navbar';
+
+//Css
+import './components/navbar/style/navbar.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import 'flexboxgrid';
+
+class Navbar extends React.Component{
+    render(){
+ return(
+     <div id='navbar'>
+         <ul id="nav" className='row center-xs middle-xs'>
+             <li className='col-xs-2'><a href="#FingerFood">Finger Food</a></li>
+             <li className='col-xs-2'><a href="#Snacks">Snacks</a></li>
+             <li className='col-xs-2'><a href="#Create">Create</a></li>
+             <li className='col-xs-2'><a href="#Desserts">Desserts</a></li>
+             <li className='col-xs-2'><a href="#Drinks">Drinks</a></li>
+         </ul>
+     </div>
+ )
+
+    }
+}
+
+export default Navbar;
